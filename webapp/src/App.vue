2bb8f2f7ef45a18759557c31d2637ed0449862c9
Jan <ja.dorn@web.de>
2024-11-23 22:16:33 +0100

    coloring and layout adjustments

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -12,7 +12,7 @@ const chatLogStore = useChatLogStore();
 </script>
 
 <template>
-  <div class="mx-4 md:mx-8 lg:mx-16 xl:mx-32 2xl:mx-64 max-w-7xl mb-16">
+  <div class="mx-4 md:mx-8 lg:mx-16 xl:mx-32 2xl:mx-64 max-w-7xl pb-16 min-h-screen">
     <header class="w-full pt-6 h-16 border-slate-200 flex items-center px-4 text-2xl font-black">
       <div class="flex flex-row items-center space-x-2">
         <div>Mistravel.ai</div>
@@ -25,8 +25,8 @@ const chatLogStore = useChatLogStore();
           Plan and Book <br>
           Your Dream Vacation 
           <span class="relative">
-            <span class="relative z-10 text-white">&nbsp; in Minutes</span>
-            <div class="absolute bg-[#3C82F6] w-48 -rotate-1 h-12 -top-0 left-1 z-0"></div>
+            <span class="relative z-10 text-white whitespace-nowrap">&nbsp;in Minutes</span>
+            <div class="absolute bg-[#3C82F6] w-48 -rotate-1 h-12 -top-0 left-0 z-0"></div>
           </span>
         </div>
       </div>
