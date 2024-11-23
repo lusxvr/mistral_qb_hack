dc5ccfa1698a4ebab1faf1d0b5c6679fe82f5fba
Luis <luis.wiedmann@gmail.com>
2024-11-23 18:11:05 +0100

    new agent with memory and internet access

--- backend/agent.py
+++ backend/agent.py
@@ -1,57 +1,47 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from typing import Annotated
-from typing_extensions import TypedDict
+from system_prompt import prompt 
+
+from datetime import datetime
 
 from langchain_mistralai import ChatMistralAI
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
+from langchain_core.tools import tool
+from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.store.memory import InMemoryStore
+from langchain_community.tools.tavily_search import TavilySearchResults
 
+def agent():
+    search_tool = TavilySearchResults(max_results=2)
+    def datetime_tool():
+        """Returns the current date and time"""
+        return datetime.now().isoformat()
 
-class State(TypedDict):
-    # Messages have the type "list". The `add_messages` function
-    # in the annotation defines how this state key should be updated
-    # (in this case, it appends messages to the list, rather than overwriting them)
-    messages: Annotated[list, add_messages]
+    tools = [search_tool, datetime_tool]
 
+    llm = ChatMistralAI(model="mistral-small-latest")
+    config = {"configurable": {"thread_id": "thread-1"}}
+    graph = create_react_agent(llm, tools, state_modifier=prompt, checkpointer=MemorySaver())
 
-graph_builder = StateGraph(State)
-
-llm = ChatMistralAI(model="mistral-small-latest")
-
-
-def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
-
-
-# The first argument is the unique node name
-# The second argument is the function or object that will be called whenever
-# the node is used.
-graph_builder.add_node("chatbot", chatbot)
-
-graph_builder.add_edge(START, "chatbot")
-graph_builder.add_edge("chatbot", END)
-
-graph = graph_builder.compile()
-
-def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [("user", user_input)]}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
-
-
-while True:
-    try:
-        user_input = input("User: ")
-        if user_input.lower() in ["quit", "exit", "q"]:
-            print("Goodbye!")
+    while True:
+        try:
+            user_input = {"messages": [("user", input("User: "))]}
+            print_stream(graph, user_input, config)
+            #stream_graph_updates(user_input)
+        except:
+            # fallback if input() is not available
+            print("\nSorry incorrect input")
             break
 
-        stream_graph_updates(user_input)
-    except:
-        # fallback if input() is not available
-        user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
-        stream_graph_updates(user_input)
-        break
+def print_stream(graph, inputs, config):
+    for s in graph.stream(inputs, config, stream_mode="values"):
+        message = s["messages"][-1]
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.pretty_print()
+
+agent()
+
