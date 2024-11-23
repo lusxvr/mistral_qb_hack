6901235b4efea74b5ed8a2c5eea1f4ef1caa8f34
Jan <ja.dorn@web.de>
2024-11-23 18:33:52 +0100

    add filter to main

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,16 +1,15 @@
 <script setup>
 import { ref, onMounted, computed, watch } from 'vue';
 
-import DateRange from '@/interaction/DateRange.vue'
-import Price from '@/interaction/Price.vue'
-import Vibe from '@/interaction/Vibe.vue'
-import Chat from '@/interaction/Chat.vue'
-import TravelMedium from '@/interaction/TravelMedium.vue';
-import TravelTime from '@/interaction/TravelTime.vue';
+import { useUserInfoStore } from '@/pinia/userInfo';
+import Filter from '@/interaction/Filter.vue';
+import Chat from '@/interaction/TextInput.vue'
+
+const userInfoStore = useUserInfoStore();
 </script>
 
 <template>
-  <div class="mx-4 md:mx-8 lg:mx-16 xl:mx-32 2xl:mx-64 max-w-7xl">
+  <div class="mx-4 md:mx-8 lg:mx-16 xl:mx-32 2xl:mx-64 max-w-7xl mb-16">
     <header class="w-full h-16 border-b border-slate-200 flex items-center px-4 text-2xl font-bold">
       Mistravel.ai
     </header>
@@ -19,13 +18,7 @@ import TravelTime from '@/interaction/TravelTime.vue';
         <div class="text-4xl font-bold">Plan and Book <br>Your Dream Vacation in Minutes</div>
       </div>
       <div class="flex flex-col space-y-2 items-start justify-center h-full">
-        <div class="w-full flex flex-col space-y-4 mb-4 items-center">
-          <DateRange />
-          <Price class="w-64" />
-          <Vibe />
-          <TravelMedium />
-          <TravelTime />
-        </div>
+        <Filter />
         <Chat />
       </div>
     </div>
