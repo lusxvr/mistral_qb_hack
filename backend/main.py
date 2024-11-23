c1576deabbfc80aa5ca4022002e10893bba319c8
Luis <luis.wiedmann@gmail.com>
2024-11-23 23:35:27 +0100

    first working chat (no state memory)

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,27 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from . import agent
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class UserInput(BaseModel):
     input: str
