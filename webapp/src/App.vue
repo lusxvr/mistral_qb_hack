cefaa2a02c84c702eb2284230fa999ae2a2dca73
Jan <ja.dorn@web.de>
2024-11-23 17:07:14 +0100

    add all new components to main

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { ref, onMounted, computed, watch } from 'vue';
 
-import { Button } from '@/components/ui/button'
-import { Textarea } from '@/components/ui/textarea'
-import DateRange from '@/select/DateRange.vue'
-import Price from '@/select/Price.vue'
+import DateRange from '@/interaction/DateRange.vue'
+import Price from '@/interaction/Price.vue'
+import Vibe from '@/interaction/Vibe.vue'
+import Chat from '@/interaction/Chat.vue'
+import TravelMedium from '@/interaction/TravelMedium.vue';
+import TravelTime from '@/interaction/TravelTime.vue';
 </script>
 
 <template>
@@ -12,19 +14,19 @@ import Price from '@/select/Price.vue'
     <header class="w-full h-16 border-b border-slate-200 flex items-center px-4 text-2xl font-bold">
       Mistravel.ai
     </header>
-    <div class="mt-8 mx-4 md:mx-16 lg:mx-48 xl:mx-48 2xl:mx-64 text-center">
+    <div class="mt-8 mx-4 md:mx-16 lg:mx-48 2xl:mx-64 text-center">
       <div class="mb-8">
         <div class="text-4xl font-bold">Plan and Book <br>Your Dream Vacation in Minutes</div>
       </div>
       <div class="flex flex-col space-y-2 items-start justify-center h-full">
-        <div class="w-full flex flex-row space-x-4 mb-4 items-center">
+        <div class="w-full flex flex-col space-y-4 mb-4 items-center">
           <DateRange />
           <Price class="w-64" />
+          <Vibe />
+          <TravelMedium />
+          <TravelTime />
         </div>
-        <Textarea class="min-h-48" />
-        <div class="w-full flex justify-end">
-          <Button variant="default">See vacation plans</Button>
-        </div>
+        <Chat />
       </div>
     </div>
   </div>
