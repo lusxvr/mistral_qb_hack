a0fa92c3c6cc803798a0c64c2a55394c031a9b76
Jan <ja.dorn@web.de>
2024-11-23 18:33:02 +0100

    added pinia store for storing user info and text input

---
+++ webapp/src/pinia/userInfo.js
@@ -0,0 +1,60 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useUserInfoStore = defineStore('userInfo', () => {
+  // State
+  const filter = ref({
+    date: {
+      start: null,
+      end: null
+    },
+    maxPrice: null,
+    vibe: [],
+    travelMedium: [],
+    maxTravelTime: null
+  })
+  const userInput = ref([])
+
+  // Actions
+  const setDate = (newDate) => {
+    filter.value.date = newDate
+  }
+
+  const setMaxPrice = (price) => {
+    filter.value.maxPrice = price
+  }
+
+  const setVibe = (newVibe) => {
+    filter.value.vibe = newVibe
+  }
+
+  const setTravelMedium = (medium) => {
+    filter.value.travelMedium = medium
+  }
+
+  const setMaxTravelTime = (time) => {
+    filter.value.maxTravelTime = time
+  }
+
+  const addUserInput = (input) => {
+    userInput.value.push(input)
+  }
+
+  const removeUserInput = (index) => {
+    userInput.value.splice(index, 1)
+  }
+
+  return {
+    // State
+    filter,
+    userInput,
+    // Actions
+    setDate,
+    setMaxPrice,
+    setVibe,
+    setTravelMedium,
+    setMaxTravelTime,
+    addUserInput,
+    removeUserInput
+  }
+})
