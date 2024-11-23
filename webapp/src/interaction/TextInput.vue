620badc4e4bb17c4d569a01271a147d2aad66804
Jan <ja.dorn@web.de>
2024-11-23 19:19:46 +0100

    add user message to log upon button press

--- webapp/src/interaction/TextInput.vue
+++ webapp/src/interaction/TextInput.vue
@@ -1,11 +1,24 @@
 <script setup>
 import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
+import { useChatLogStore } from '@/pinia/chatLog';
+
+import { ref } from 'vue';
+
+const chatLogStore = useChatLogStore();
+const inputValue = ref('');
+
+const handleSubmit = () => {
+    if (inputValue.value.trim()) {
+        chatLogStore.addMessage(inputValue.value, true)
+        inputValue.value = ''
+    }
+}
 </script>
 
 <template>
-    <Textarea placeholder="What else should I consider?" class="min-h-48" />
+    <Textarea v-model="inputValue" placeholder="What else should I consider?" class="min-h-48" />
     <div class="w-full flex justify-end">
-        <Button variant="default">See vacation plans</Button>
+        <Button variant="default" @click="handleSubmit">See vacation plans</Button>
     </div>
 </template>
